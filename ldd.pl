3af1804506d0c2b927b025a47a9446ed9859b999
doremi <doremi@ubuntu>
2012-03-20 15:52:03 +0800

    用來做類似 ldd 的事情
    使用方法:
    
    arm-eabi-readelf -d ./exe | ldd.pl
    
    這樣會列出該 .so 是否存在於 $OUT/system/lib

---
+++ ldd.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use constant DEFAULT_PATH => "/home/LinDoremi/scorpio-devel/out/target/product/scorpio/system/lib/";
+
+sub check_file
+{
+    my $file;
+    my ($f) = @_;
+
+    if ($ENV{'OUT'}) {
+        $file = "$ENV{'OUT'}/system/lib/$f";
+    } else {
+        $file = DEFAULT_PATH."$f";
+    }
+
+    if (-e $file) {
+        print "Check $file OK!\n";
+    } else {
+        print "Check \033[1;31m$file\033[0;38m not exist!\n";
+    }
+}
+
+BEGIN
+{
+    if ($ENV{'OUT'}) {
+        print "Use $ENV{'OUT'} for search library\n";
+    } else {
+        print "No \$OUT, use default path: ", DEFAULT_PATH, "\n";
+    }
+
+    while (my $line = <STDIN>) {
+        if ($line =~ /\[(.*)\]/) {
+            check_file($1);
+        }
+    }
+
+    
+}
